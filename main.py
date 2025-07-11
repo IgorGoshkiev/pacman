9de2fce7867ec77f52b2f78693d939817625c4f5
goshkiev_igor <igor.goshkiev@yandex.ru>
2025-07-11 14:01:35 +0300

    fix pacman

--- main.py
+++ main.py
@@ -71,36 +71,59 @@ class Pacman:
                 self.mouth_angle -= self.mouth_change
                 if self.mouth_angle <= 0:
                     self.mouth_open = True
+        else:
+            # Если не двигается, рот должен быть закрыт
+            self.mouth_angle = 0
 
     def draw(self, surface):
         # Рисуем Pacman
-        start_angle = self.mouth_angle / 180 * 3.14
-        end_angle = (360 - self.mouth_angle) / 180 * 3.14
+        if self.mouth_angle == 0:  # Если рот закрыт - просто круг
+            pygame.draw.circle(surface, YELLOW, (self.x, self.y), self.radius)
+        else:
+            # Углы для дуги (в радианах)
+            if self.direction == 0:  # right
+                start_angle = self.mouth_angle * 3.14 / 180
+                end_angle = (360 - self.mouth_angle) * 3.14 / 180
+            elif self.direction == 1:  # left
+                start_angle = (180 + self.mouth_angle) * 3.14 / 180
+                end_angle = (180 - self.mouth_angle) * 3.14 / 180
+            elif self.direction == 2:  # up
+                start_angle = (270 + self.mouth_angle) * 3.14 / 180
+                end_angle = (270 - self.mouth_angle) * 3.14 / 180
+            elif self.direction == 3:  # down
+                start_angle = (90 + self.mouth_angle) * 3.14 / 180
+                end_angle = (90 - self.mouth_angle) * 3.14 / 180
 
-        if self.direction == 0:  # right
+            # Точки для линий рта
+            points = []
+            points.append((self.x, self.y))  # Центр
+
+            # Первая точка рта
+            if self.direction == 0:  # right
+                points.append((self.x + self.radius, self.y - self.mouth_angle / 2))
+            elif self.direction == 1:  # left
+                points.append((self.x - self.radius, self.y - self.mouth_angle / 2))
+            elif self.direction == 2:  # up
+                points.append((self.x - self.mouth_angle / 2, self.y - self.radius))
+            elif self.direction == 3:  # down
+                points.append((self.x - self.mouth_angle / 2, self.y + self.radius))
+
+            # Вторая точка рта
+            if self.direction == 0:  # right
+                points.append((self.x + self.radius, self.y + self.mouth_angle / 2))
+            elif self.direction == 1:  # left
+                points.append((self.x - self.radius, self.y + self.mouth_angle / 2))
+            elif self.direction == 2:  # up
+                points.append((self.x + self.mouth_angle / 2, self.y - self.radius))
+            elif self.direction == 3:  # down
+                points.append((self.x + self.mouth_angle / 2, self.y + self.radius))
+
+            # Рисуем заполненный сектор
+            pygame.draw.polygon(surface, YELLOW, points)
             pygame.draw.arc(surface, BLACK,
                             (self.x - self.radius, self.y - self.radius,
                              self.radius * 2, self.radius * 2),
                             start_angle, end_angle, 2)
-            pygame.draw.circle(surface, YELLOW, (self.x, self.y), self.radius)
-        elif self.direction == 1:  # left
-            pygame.draw.arc(surface, BLACK,
-                            (self.x - self.radius, self.y - self.radius,
-                             self.radius * 2, self.radius * 2),
-                            start_angle + 3.14, end_angle + 3.14, 2)
-            pygame.draw.circle(surface, YELLOW, (self.x, self.y), self.radius)
-        elif self.direction == 2:  # up
-            pygame.draw.arc(surface, BLACK,
-                            (self.x - self.radius, self.y - self.radius,
-                             self.radius * 2, self.radius * 2),
-                            start_angle + 4.71, end_angle + 4.71, 2)
-            pygame.draw.circle(surface, YELLOW, (self.x, self.y), self.radius)
-        elif self.direction == 3:  # down
-            pygame.draw.arc(surface, BLACK,
-                            (self.x - self.radius, self.y - self.radius,
-                             self.radius * 2, self.radius * 2),
-                            start_angle + 1.57, end_angle + 1.57, 2)
-            pygame.draw.circle(surface, YELLOW, (self.x, self.y), self.radius)
 
         # Глаз
         eye_offset_x, eye_offset_y = 0, 0
@@ -176,11 +199,14 @@ class Game:
         self.font = pygame.font.SysFont(None, 36)
         self.game_time = pygame.time.get_ticks()
 
+    def quit_game(self):
+        pygame.quit()
+        sys.exit()
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+                self.quit_game()
 
             if event.type == KEYDOWN:
                 if event.key == K_LEFT:
@@ -195,6 +221,8 @@ class Game:
                 elif event.key == K_DOWN:
                     self.pacman.direction = 3
                     self.pacman.moving = True
+                elif event.key == K_ESCAPE:  # выход по ESC
+                    self.quit_game()
 
             if event.type == KEYUP:
                 if event.key in [K_LEFT, K_RIGHT, K_UP, K_DOWN]:
